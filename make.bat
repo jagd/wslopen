6149130aa543eabf9a163b0bd8d8818a830f582b
Chuanren Wu <contact@thedrwu.com>
2022-05-29 11:00:13 +0200

    Use "echo off" in make.bat

--- make.bat
+++ make.bat
@@ -1,5 +1,6 @@
-@ cl /permissive- /GS /GL /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /sdl /Zc:inline /fp:precise /WX- /Zc:forScope /Gd /Oi /MD /FC /EHsc /nologo /DUNICODE /c main.cc
-@
-@ link  /OUT:"open.exe" /MANIFEST /NXCOMPAT /DYNAMICBASE "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" /DEBUG:FULL /MACHINE:X64 /OPT:REF /SUBSYSTEM:CONSOLE /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /OPT:ICF /TLBID:1 main.obj
-@
-@ REM /SUBSYSTEM:WINDOWS for WinMain()
+@echo off
+cl /permissive- /GS /GL /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /sdl /Zc:inline /fp:precise /WX- /Zc:forScope /Gd /Oi /MD /FC /EHsc /nologo /DUNICODE /c main.cc
+
+link  /OUT:"open.exe" /MANIFEST /NXCOMPAT /DYNAMICBASE "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" /DEBUG:FULL /MACHINE:X64 /OPT:REF /SUBSYSTEM:CONSOLE /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /OPT:ICF /TLBID:1 main.obj
+
+REM /SUBSYSTEM:WINDOWS for WinMain()
